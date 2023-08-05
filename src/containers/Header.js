6bb8efe0a87dbe6c9f4b5e11853b6287eb39b309
SheenamGoel <goelsheenam469@gmail.com>
2023-08-05 13:47:26 +0530

    footer cards by Sheenam

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import './css/Header.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-import { faShoppingBasket} from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./css/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
+const Header = () => {
 
-function Header() {
   return (
-    <div className='header'>
-
+    <div className="header">
       <div className="logo">
         <div className="solis">SOLiS</div>
         <div className="cattle-feed">Cattle Feed</div>
       </div>
 
       <div className="search">
-        <input className='search-input' type="text" placeholder='आइटम खोजें' />
+        <input
+          className="search-input"
+          type="text"
+          placeholder="आइटम खोजें"
+        />
+
         {/* searchIcon */}
-        <FontAwesomeIcon className='search-icon' icon={faMagnifyingGlass} />
+        <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
       </div>
 
       <div className="cart">
         {/* basketIcon */}
-        <FontAwesomeIcon className='basket-icon' icon={faShoppingBasket} />
+        <FontAwesomeIcon className="basket-icon" icon={faShoppingBasket} />
         <div className="my-cart">My Basket</div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
